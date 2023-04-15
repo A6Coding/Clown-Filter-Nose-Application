3c35d7aa6a2611dfc46620d0721455f52303411a
A6Coding <111938126+A6Coding@users.noreply.github.com>
2023-04-14 20:23:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(400, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(600, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelReady);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelReady() {
+    console.log("poseNet is ready!")
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 400, 300);
+}
+
+function take_snapshot() {
+    save("my_clown_picture.png");
+}
